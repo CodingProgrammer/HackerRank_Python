9dd91b082586e66bdd6d942609185e408d578741
Unknown <157136443@qq.com>
2017-12-17 18:54:31 +0800

    (All)Find_a_string
    1.The first index
    2.All index
    3.The number of the times that the substring occurs in the given string

---
+++ (All)Find_a_string.py
@@ -0,0 +1,16 @@
+def count_substring(string, sub_string):
+    len_string = len(string)
+    len_sub = len(sub_string)
+    n = sum([1 for i in range(len_string - len_sub + 1) if string[i:i + len_sub] == sub_string]) 
+    return n
+'''
+l = [i for i in range(len_string - len_sub + 1) if string[i:i + len_sub] == sub_string]
+1.If you want to find the first index of the substring in the original string
+l[0] is your destination
+
+2.If you want to find every index of the snstring in the original
+l[::] is what you want
+
+3.If you want the number of times that the substring occurs in the given string
+len(l) is what you want
+'''
