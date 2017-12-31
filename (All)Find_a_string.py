b79aa984d7bff918c8fb5dce78642e23a4bf2551
Unknown <157136443@qq.com>
2017-12-31 12:46:52 +0800

    (All)Find_a_String
    add a recursion operation

--- (All)Find_a_string.py
+++ (All)Find_a_string.py
@@ -3,6 +3,12 @@ def count_substring(string, sub_string):
     len_sub = len(sub_string)
     n = sum([1 for i in range(len_string - len_sub + 1) if string[i:i + len_sub] == sub_string]) 
     return n
+
+def count_substring(string, sub_string): #use recursion and a built in function
+    if string.find(sub_string) < 0:
+        return 0
+    else:
+        return 1 + count_substring(string[string.find(sub_string) + 1:], sub_string)
 '''
 l = [i for i in range(len_string - len_sub + 1) if string[i:i + len_sub] == sub_string]
 1.If you want to find the first index of the substring in the original string
