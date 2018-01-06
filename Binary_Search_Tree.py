c9c1071f922519effa68b883d0eb46f739368ec6
Unknown <157136443@qq.com>
2018-01-06 16:46:23 +0800

    Binary_Search_Tree
    level_order_iteration & level_order_recursion

--- Binary_Search_Tree.py
+++ Binary_Search_Tree.py
@@ -23,7 +23,7 @@ class Solution:
             height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
             return height
     
-    def levelOrder(self, root):
+    def levelOrder_Iteration(self, root):
         if root == None:
             return 
         else:
@@ -38,7 +38,17 @@ class Solution:
                     if node.right:
                         myqueue.append(node.right)
 
-
+    def levelOrder_Recursion(self, root):
+        ans = []
+        def preOrder(root, level):
+            if root:
+                if len(ans) < level + 1:
+                    ans.append([])
+                ans[level].append(root.data)       #change the sequence here can get pre_order, mid_order
+                preOrder(root.left, level + 1)
+                preOrder(root.right, level + 1)
+        preOrder(root, 0)
+        print(ans)
 T = 9
 l = [20, 50, 35, 44, 9, 15, 62, 11, 13]
 myTree = Solution()
@@ -48,4 +58,5 @@ for i in range(T):
     root = myTree.insert(root,data)
 height = myTree.getHeight(root)
 print(height)  
-myTree.levelOrder(root)
+myTree.levelOrder_Iteration(root)
+myTree.levelOrder_Recursion(root)
