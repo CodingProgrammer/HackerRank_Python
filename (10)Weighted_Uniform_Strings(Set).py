497f1942462b7fcb472a35dfdca29aadeff49ce2
Unknown <157136443@qq.com>
2018-01-14 23:18:58 +0800

    (10)Weighted_Uniform_Strings(Set)
    Awesome use of set to same time

---
+++ (10)Weighted_Uniform_Strings(Set).py
@@ -0,0 +1,31 @@
+'''
+The difference between this one with TimeOut one is "res = set()" here
+'''
+#!/bin/python3
+
+import sys
+
+d = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26}
+s = input().strip()
+n = int(input().strip())
+mystack = []
+level = 0
+res = set()
+for i, c in enumerate(s):
+    if i == 0:
+        mystack.append([])
+        mystack[level].append(c)
+    elif c != s[i - 1]:
+        level += 1
+        mystack.append([])
+        mystack[level].append(c)
+    else:
+        mystack[level].append(c)
+for i in range(len(mystack)):
+    num = 0
+    for each in mystack[i]:
+        num += d[each]
+        res.add(num)
+for a0 in range(n):
+    x = int(input().strip())
+    print('Yes' if x in res else 'No')
