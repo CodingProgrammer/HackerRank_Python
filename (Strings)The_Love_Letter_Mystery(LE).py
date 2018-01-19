f1ae90581b61b63b60c6bbdb219b4b1782bb4cd0
Unknown <157136443@qq.com>
2018-01-19 10:46:59 +0800

    (Strings)The_Love_Letter_Mystery(LE)
    List_Expression

---
+++ (Strings)The_Love_Letter_Mystery(LE).py
@@ -0,0 +1,19 @@
+'''
+List_Expression
+'''
+#!/bin/python3
+
+import sys
+
+def theLoveLetterMystery(s):
+    # Complete this function
+    if s == s[::-1]:
+        return 0
+    length = len(s)
+    res = 0
+    return sum([abs(ord(s[i]) - ord(s[length - i - 1])) for i in range(0, length // 2) if s[i] != s[length - i - 1]])
+q = int(input().strip())
+for a0 in range(q):
+    s = input().strip()
+    result = theLoveLetterMystery(s)
+    print(result)
