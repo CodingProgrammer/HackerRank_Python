3a8285e71e4b00dab16f36a5e3b87ad5fd9ad3e2
Unknown <157136443@qq.com>
2018-01-22 15:26:59 +0800

    (Sorting)Insertion_Sort_Part1
    add a while-loop method

--- (Sorting)Insertion_Sort_Part1.py
+++ (Sorting)Insertion_Sort_Part1.py
@@ -14,7 +14,19 @@ def insertionSort1(n, arr):
             arr[i + 1] = temp
             print(' '.join(map(str, arr)))
             break
-
+'''
+#another method use while loop
+def insertionSort1(n, arr):
+    # Complete this function
+    temp = arr[-1]
+    i = n - 2
+    while arr[i] > temp and i >= 0:
+        arr[i + 1] = arr[i]
+        i -= 1
+        print(' '.join(map(str, arr)))
+    arr[i + 1] = temp 
+    print(' '.join(map(str, arr))) 
+'''
 if __name__ == "__main__":
     n = int(input().strip())
     arr = list(map(int, input().strip().split(' ')))
