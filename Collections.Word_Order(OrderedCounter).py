673872ebd4f76afcf5f2ccfda108411aec9b3a72
Unknown <157136443@qq.com>
2018-03-11 15:08:57 +0800

    Collections.Word_Order(OrderedCounter)
    Awesome creating OrderedCounter-class

---
+++ Collections.Word_Order(OrderedCounter).py
@@ -0,0 +1,9 @@
+'''
+Awesome creating OrderedCounter-class
+'''
+from collections import OrderedDict, Counter
+class OrderedCounter(Counter, OrderedDict):
+    pass
+result = OrderedCounter(input() for _ in range(int(input())))
+print(len(result))
+print(*result.values())
