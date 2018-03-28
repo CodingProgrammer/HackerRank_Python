60c9d9bad48a05c94efe19c0ec90a965a3edb301
Unknown <157136443@qq.com>
2018-03-28 16:18:59 +0800

    (Greedy)Largest_Permutation

---
+++ (Greedy)Largest_Permutation.py
@@ -0,0 +1,34 @@
+'''
+To solve the timeout problem, use a dict to find the wanted elements to save time
+But remember to change the index also when do the swap operation
+'''
+#!/bin/python3
+
+import sys
+
+def largestPermutation(n, k, arr):
+    num_swap = 0
+    table = {}
+    for i in range(n):
+        table[str(arr[i])] = i
+
+    for i in range(n):
+        if num_swap >= k:
+            break
+
+        if arr[i] != (n - i):
+            temp =arr[i]
+            temp_pos = table[str(n-i)]
+            arr[i], arr[table[str(n-i)]] = arr[table[str(n-i)]], arr[i]
+            table[str(arr[i])], table[str(temp)] = i, temp_pos
+            num_swap += 1
+    return arr
+
+
+
+if __name__ == "__main__":
+    n, k = input().strip().split(' ')
+    n, k = [int(n), int(k)]
+    arr = list(map(int, input().strip().split(' ')))
+    result = largestPermutation(n, k, arr)
+    print (" ".join(map(str, result)))
