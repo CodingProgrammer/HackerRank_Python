c71031b6cc44ad18e3da9dc4d0c90877fc787934
Unknown <157136443@qq.com>
2018-04-01 22:33:22 +0800

    (Greedy)Chief_Hopper(66%)

---
+++ (Greedy)Chief_Hopper(66%).py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import sys
+from itertools import count
+def chiefHopper(arr):
+    
+    for botEnergy in count(arr[0]):
+        temp = botEnergy
+        for each_height in arr:
+            if each_height > botEnergy:
+                botEnergy -= (each_height - botEnergy)
+            else:
+                botEnergy += (botEnergy - each_height)
+            if botEnergy < 0:
+                break
+
+        if botEnergy > 0:
+            return temp
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    result = chiefHopper(arr)
+    print(result)
