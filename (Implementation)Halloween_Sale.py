b21807510d39b5d470eab43490816da8d6fcc04a
Jason <157136443@qq.com>
2018-05-14 18:56:04 +0800

    (Implementation)Halloween_Sale.py

---
+++ (Implementation)Halloween_Sale.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the howManyGames function below.
+def howManyGames(p, d, m, s):
+    ans = 0
+    current_price = p
+    while (s - current_price) >= 0:
+        s -= current_price
+        ans += 1
+        if current_price - d > m:
+            current_price -= d
+        else:
+            current_price = m
+    return ans   
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    pdms = input().split()
+
+    p = int(pdms[0])
+
+    d = int(pdms[1])
+
+    m = int(pdms[2])
+
+    s = int(pdms[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
